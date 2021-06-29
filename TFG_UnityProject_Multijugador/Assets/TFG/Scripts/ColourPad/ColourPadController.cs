bb15ffdd210eac82b4a1348a4ba7d2c159c9af21
pserrano778 <58224411+pserrano778@users.noreply.github.com>
2021-06-30 01:06:44 +0200

    puzzle cocina con cajones de los muebles

---
+++ TFG_UnityProject_Multijugador/Assets/TFG/Scripts/ColourPad/ColourPadController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ColourPadController : MonoBehaviour
+{
+    private char color;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        color = 'b';
+        setNewColor(Color.blue);
+    }
+
+    public void NextColor()
+    {
+        if (color == 'b'){
+            color = 'r';
+            setNewColor(Color.red);
+        }
+        else if (color == 'r')
+        {
+            color = 'g';
+            setNewColor(Color.green);
+        }
+        else
+        {
+            color = 'b';
+            setNewColor(Color.blue);
+        }
+    }
+
+    private void setNewColor(Color color)
+    {
+        var newColors = GetComponent<Button>().colors;
+
+        newColors.normalColor = color;
+
+        var transparentColor = color;
+        transparentColor.a = 0.6f;
+
+        newColors.highlightedColor = transparentColor;
+
+        GetComponent<Button>().colors = newColors;
+    }
+
+    public char getColor()
+    {
+        return color;
+    }
+}
---
+++ TFG_UnityProject_Multijugador/Assets/TFG/Scripts/ColourPad/ColourPadEventController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColourPadEventController : MonoBehaviour
+{
+    public string str;
+    public ColourPadController[] colours;
+    public Animator animator;
+    public GameObject objectInside;
+
+    private bool locked = true;
+
+    private void Start()
+    {
+        objectInside.SetActive(false);
+    }
+    public void CheckCode()
+    {
+        if (locked)
+        {
+            string currentStr = "";
+
+            for (int i=0; i< colours.Length; i++)
+            {
+                currentStr += colours[i].getColor();
+            }
+
+            if (str.ToString().Equals(currentStr)){
+               PlayAnimation();
+            }
+
+            locked = true;
+        }
+    }
+
+    private void PlayAnimation()
+    {
+        print("anim");
+        objectInside.SetActive(true);
+        animator.Play("open", 0, 0.0f);
+    }
+}
