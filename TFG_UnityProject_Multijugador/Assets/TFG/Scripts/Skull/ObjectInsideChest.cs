8538b7f0a795a650d5a93cab82672e552eabec0a
pserrano778 <58224411+pserrano778@users.noreply.github.com>
2021-07-20 13:10:21 +0200

    mejora primer puzzle multijugador

---
+++ TFG_UnityProject_Multijugador/Assets/TFG/Scripts/Skull/ObjectInsideChest.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class ObjectInsideChest : MonoBehaviour
+{
+    public SkullBehaviour boneTracker;
+    public string tagToCompare;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(tagToCompare))
+        {
+            if (other.gameObject.GetComponent<PhotonView>() != null && other.gameObject.GetComponent<PhotonView>().IsMine)
+            {
+                GetComponent<PhotonView>().RPC("setObjectInside", RpcTarget.All);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.GetComponent<PhotonView>() != null && other.gameObject.GetComponent<PhotonView>().IsMine)
+        {
+            GetComponent<PhotonView>().RPC("setObjectOutside", RpcTarget.All);
+        }
+    }
+
+    [PunRPC]
+    private void setObjectInside()
+    {
+        boneTracker.ChangeObjectInside(true);
+    }
+
+    [PunRPC]
+    private void setObjectOutside() {
+        boneTracker.ChangeObjectInside(false);
+    }
+}
---
+++ TFG_UnityProject_Multijugador/Assets/TFG/Scripts/Skull/SkullBehaviour.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class SkullBehaviour : MonoBehaviour
+{
+    public Animator chestAnimator;
+    public GameObject note;
+    private bool objectInside = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        note.GetComponent<VRTK_InteractableObject>().isGrabbable = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Bone")
+        {
+            chestAnimator.Play("open", 0, 0.0f);
+            note.GetComponent<VRTK_InteractableObject>().isGrabbable = true;
+            StartCoroutine(WaitForAnimation(chestAnimator.GetCurrentAnimatorStateInfo(0).length * chestAnimator.GetCurrentAnimatorStateInfo(0).speed));
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Bone")
+        {
+            chestAnimator.Play("close", 0, 0.0f);
+            StartCoroutine(WaitForAnimation(chestAnimator.GetCurrentAnimatorStateInfo(0).length * chestAnimator.GetCurrentAnimatorStateInfo(0).speed));
+            if (objectInside && !note.GetComponent<Rigidbody>().isKinematic)
+            {
+                note.GetComponent<VRTK_InteractableObject>().isGrabbable = false;
+            }
+        }
+    }
+
+    IEnumerator WaitForAnimation(float time)
+    {
+        yield return new WaitForSeconds(time);
+    }
+
+    public void ChangeObjectInside(bool isObjectInside)
+    {
+        objectInside = isObjectInside;
+    }
+}
