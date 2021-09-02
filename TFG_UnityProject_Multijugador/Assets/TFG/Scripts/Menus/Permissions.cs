ab7dcbcf7c1f1714cfcc92d466f5ed01e0c285b4
pserrano778 <58224411+pserrano778@users.noreply.github.com>
2021-09-02 20:55:55 +0200

    Ajustes iluminación

---
+++ TFG_UnityProject_Multijugador/Assets/TFG/Scripts/Menus/Permissions.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Android;
+
+public class Permissions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Micrófono
+        if (!Permission.HasUserAuthorizedPermission(Permission.Microphone))
+        {
+            Permission.RequestUserPermission(Permission.Microphone);
+        }
+
+        // Lectura
+        if (!Permission.HasUserAuthorizedPermission(Permission.ExternalStorageRead))
+        {
+            Permission.RequestUserPermission(Permission.ExternalStorageRead);
+        }
+
+        // Escritura
+        if (!Permission.HasUserAuthorizedPermission(Permission.ExternalStorageWrite))
+        {
+            Permission.RequestUserPermission(Permission.ExternalStorageWrite);
+        }
+    }
+
+}
